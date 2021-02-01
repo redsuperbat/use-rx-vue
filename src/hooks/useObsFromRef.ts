fd06359399cc9d5061bd6d08affc79e2c0e2f883
redsuperbat <max.netterberg@gmail.com>
2021-02-02 00:33:14 +0100

    changed subject to behavioursubject in useObsFromRef

--- src/hooks/useObsFromRef.ts
+++ src/hooks/useObsFromRef.ts
@@ -1,8 +1,8 @@
-import { Subject } from "rxjs";
+import { BehaviorSubject } from "rxjs";
 import { Ref, watch } from "vue";
 
 export const useObsFromRef = <T>(ref: Ref<T>) => {
-  const subject = new Subject<T>();
+  const subject = new BehaviorSubject<T>(ref.value);
   watch(ref, (value) => {
     subject.next(value);
   });
