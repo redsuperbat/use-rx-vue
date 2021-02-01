477fc05c1b474709a4a20d2a2b3e2acd4f16e21f
redsuperbat <max.netterberg@gmail.com>
2021-02-02 00:45:43 +0100

    forgot export statement

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { useObservable } from "./hooks/useObservable";
 export { useObsFromEvent } from "./hooks/useObsFromEvent";
 export { useObsFromRef } from "./hooks/useObsFromRef";
 export { useModelValue } from "./hooks/useModelValue";
+export { useObsFromReactive } from "./hooks/useObsFromReactive";
